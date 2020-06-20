299675d4ac2f41263b3acb7cf3f9226f5f0b5f0f
galimskyy <motorokz@gmail.com>
2020-06-20 21:33:55 +0500

    wave config (bad way)

--- Assets/Scripts/Components/Spawner.cs
+++ Assets/Scripts/Components/Spawner.cs
@@ -1,18 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Boo.Lang;
+using Unity.Entities.UniversalDelegates;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject nextPrefabToSpawn;
     public GameObject pathContainer;
     int counter = 0;
 
+    public LevelConfiguration levelConfig;
+
+    public PackConfiguration wave1Config;
+    public PackConfiguration wave2Config;
+    public PackConfiguration wave3Config;
+
+    private List<PackConfiguration> packConfiguration;
+
     public string targetName;
+
+    private void Awake()
+    {
+        packConfiguration = new List<PackConfiguration>()
+        {
+            wave1Config,
+            wave2Config,
+            wave3Config
+        };
+    }
+
+    private void Start()
+    {
+        
+    }
 
     public void Spawn()
     {
         counter++;
-        GameObject newObj = GameObject.Instantiate(nextPrefabToSpawn);
+  
+    }
+
+    private void SpawnWave()
+    {
+        foreach(var pack in packConfiguration)
+        {
+            for(var i =pack.npc1minValue; i <=pack.npc1addValue; i++)
+            {
+
+            }
+        }
+    }
+
+    private void SpawnNewNPC(GameObject prefab)
+    {
+        GameObject newObj = GameObject.Instantiate(prefab);
         newObj.GetComponent<BaseDamageTransmitter>().Init(GameObject.Find(targetName), newObj.transform.position);
         newObj.name = $"NPC{counter}";
         newObj.GetComponent<PathFollowerBase>().Init(pathContainer);
--- Assets/Scripts/Configuration/PackConfiguration.cs
+++ Assets/Scripts/Configuration/PackConfiguration.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections;
 
 [CreateAssetMenu(fileName = "PackConfiguration", menuName = "Configuration/PackConfiguration", order = 1)]
 public class PackConfiguration : ScriptableObject
 {
-    public GameObject NPCPrefab;
-    public int minValue;
-    public int addVaue;
+    [Header("NPC1")]
+    public GameObject NPC1;
+    public int npc1minValue;
+    public int npc1addValue;
+
+    [Header("NPC2")]
+    public GameObject NPC2;
+    public int npc2minValue;
+    public int npc2addValue;
+
+    [Header("NPC3")]
+    public GameObject NPC3;
+    public int npc3minValue;
+    public int npc3addValue;
 }
